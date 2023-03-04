cc3d7767a3924e71c6e76d639971a121192ce207
Nikola Semov <52454229+nikolasemov21@users.noreply.github.com>
2023-03-04 12:30:11 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,11 @@
+import turtle
+import functions
+
+timmy_the_turtle = turtle.Turtle()
+# functions.spirograph(timmy_the_turtle, 5)
+# functions.random_walk(timmy_the_turtle)
+# functions.draw_overlay_shapes(timmy_the_turtle, 100)
+# functions.draw_square(timmy_the_turtle, 100)
+# functions.draw_dashed_line(timmy_the_turtle, 10, 3, 5)
+screen = turtle.Screen()
+screen.exitonclick()
